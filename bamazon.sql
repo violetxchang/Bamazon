d14a7675b0cbf7d0fb3a44e1408d170fe8b8ce67
violet chang <violet7chang@gmail.com>
2019-07-15 22:02:43 -0500

    initial commit

---
+++ bamazon.sql
@@ -0,0 +1,12 @@
+DROP DATABASE IF EXISTS bamazonDB;
+CREATE database bamazonDB;
+
+USE bamazonDB;
+
+CREATE TABLE products (
+item_id INT NOT NULL,
+product_name VARCHAR(60) NOT NULL,
+department_name VARCHAR(60) NOT NULL,
+price DECIMAL(6,2) NOT NULL,
+stock_quantity INT NOT NULL
+);
