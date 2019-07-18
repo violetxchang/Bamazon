9b33359f963bf4e0ea6d9519e56b05efd5c07bee
violet chang <violet7chang@gmail.com>
2019-07-17 21:44:41 -0500

    update to bamazonCustomer.js

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Bamazon
+
+Bamazon is an Amazon-like storefront app that takes in orders from customers and depletes stock from the store's inventory using MySQL.
+
+## Technologies Used
+
+- Node.js
+- MySQL
+
+### Developer - Violet Chang
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -5,7 +5,7 @@ var connection = mysql.createConnection({
   host: "localhost",
 
   //your port
-  port: 7777,
+  port: 3306,
 
   // Your username
   user: "violetchang",
@@ -17,6 +17,33 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err) {
   if (err) throw err;
+  buyProduct();
   console.log("connected as id " + connection.threadId);
   connection.end();
 });
+
+function buyProduct(){
+  inquirer
+  .prompt({
+    name: "buy",
+    type: "list",
+    message: "What would you like to buy?",
+    choices: [
+      "Hydrating Moisturizer",
+      "Soothing Moisturizer",
+      "Enriched Body Lotion",
+      "Clear Quartz Luminizer",
+      "Amethyst Luminizer",
+      "Noni Glow Face Oil",
+      "Noni Radiant Eye Oil",
+      "Body Exfoliator",
+      "Noni Glow Sleeping Mask",
+      "Turmeric Brightening Mask"
+    ]
+  })
+  .then(function(answer){
+    switch(answer.action){
+
+    }
+  })
+}
